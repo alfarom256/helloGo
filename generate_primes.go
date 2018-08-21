993eea65c14fa9fa7d894a50cd81a75b5ffb02f7
AlfaroM <alfaro.3@wright.edu>
2018-08-20 21:58:37 -0400

    Finished (sloppy) prime generator

--- generate_primes.go
+++ generate_primes.go
@@ -10,6 +10,36 @@ import (
 )
 
 //method to generate primes
+//Params
+//max_number : int : number to count primes up to
+//primes : []int : array of primes up to max_number / 2
+func primeGen(max_number int, primes []int) []int {
+	//if max_number is odd, increment by one
+	if max_number%2 == 1 {
+		max_number++
+	}
+	fmt.Printf("testing up to %d\n", (max_number))
+	//for every odd number from 3 to max_number/2
+	for i := 3; i < max_number; i += 2 {
+		if testPrime(i, primes) {
+			fmt.Printf("%d is prime!\n", i)
+			primes = append(primes, i)
+		}
+	}
+	return primes
+}
+
+func testPrime(number int, primes []int) (is_prime bool) {
+	max_prime := (number + 1) / 2
+	for _, j := range primes {
+		if j > max_prime {
+			break
+		} else if number%j == 0 {
+			return false
+		}
+	}
+	return true
+}
 
 func main() {
 	/*
@@ -42,7 +72,11 @@ func main() {
 	max_num, err := strconv.Atoi(text)
 
 	if err == nil {
-		fmt.Printf("You entered %v", max_num)
+		fmt.Printf("You entered %v\n", max_num)
+		primes := []int{2, 3}
+		primes = primeGen(max_num, primes)
+		fmt.Printf("Primes: %v", primes)
+
 	} else {
 		fmt.Print("Can't understand, %v is type %v", max_num, reflect.TypeOf(max_num))
 	}
