48b6a79d515ed321b84fc90ffb7bf4d3b0a0758e
Michael.Alfaro <michael.alfaro@opt003613.local>
2018-08-21 00:38:26 -0400

    finished copyFile, TODO: add comments

--- copyFile.go
+++ copyFile.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+    "io/ioutil"
 )
 
 func main() {
@@ -18,7 +19,31 @@ func main() {
 	srcFile := args[0]
 	destFile := args[1]
 	fmt.Printf("Copying %s to %s\n", srcFile, destFile)
+    writeFile(destFile, readFile(srcFile))
+    fmt.Print("File written! Checking....\n")
+    writtenData := readFile(destFile)
+    fmt.Printf("this is in the destination file:\n%s\n", writtenData)
 
+}
+
+func checkErr(e error){
+    if e != nil{
+        panic(e)
+    }
+}
+
+func readFile(sourceFile string) []byte {
+   data, err := ioutil.ReadFile(sourceFile)
+   checkErr(err)
+
+   fmt.Printf("Here's what I read from %s:\n%s\n", sourceFile, data)
+   return data
+}
+
+func writeFile(destFile string,data []byte) bool {
+   err := ioutil.WriteFile(destFile, data, 0644)
+   defer checkErr(err)
+   return err != nil
 }
 
 func usage() {
